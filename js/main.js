d67b88722610dc52d53da765d0d78a1a5546d107
amberwoodrow <amberwoodrow@gmail.com>
2015-07-31 08:59:19 -0600

    mission complete

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// add scripts
+
+$(document).on('ready', function() {
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var toDo = $('input').val();
+    $('#all-todos').prepend('<li><h2>'+toDo+
+      '&nbsp;<button class="btn btn-danger btn-sm">X</button></h2></li>');
+  });
+
+  $(document).on('click', 'li', function() {
+      $(this).closest('li').remove();
+  });
+});
