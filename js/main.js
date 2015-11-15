47d7cae27bbeb9aae2fe7149a299187684928005
amberwoodrow <amberwoodrow@gmail.com>
2015-11-14 22:22:14 -0700

    empty input on submit

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).on('ready', function() {
     var toDo = $('input').val();
     $('#all-todos').prepend('<li><h2>'+toDo+
       '&nbsp;<button class="btn btn-danger btn-sm">X</button></h2></li>');
+    $('#form-input').val("");
   });
 
   $(document).on('click', 'li', function() {
